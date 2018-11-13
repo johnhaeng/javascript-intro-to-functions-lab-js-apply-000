de60b875d5761d2c14957e3fbb73b083f9092eaf
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-13 23:11:09 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -19,7 +19,11 @@ function logWhisper(string) {
 }
 
 function sayHiToGrandma(string) {
-  if (string === string.toLowerCase) {
+  if (string === string.toLowerCase()) {
     return `I can't hear you`;
-  } else if ()
+  } else if (string === string.toUpperCase()) {
+    return 'YES, INDEED!';
+  } else if (string === 'I love you') {
+    return 'Yes, I love you, too.';
+  }
 }
