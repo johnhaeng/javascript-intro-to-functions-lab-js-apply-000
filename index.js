3a441cbc750e18cafaa0cc0882d8164029bb0059
inherited-firmware-6870 <johnhaeng@gmail.com>
2018-11-13 23:10:01 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,4 +18,8 @@ function logWhisper(string) {
   console.log(newStr);
 }
 
-
+function sayHiToGrandma(string) {
+  if (string === string.toLowerCase) {
+    return `I can't hear you`;
+  } else if ()
+}
